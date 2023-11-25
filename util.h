4fff9d53b13e86b288f714905721c290188e01f1
ejpietsc <ejpietsc@uwaterloo.ca>
2023-11-24 22:48:33 -0500

    implemented operator==, made RESIGNATION_MOVE move

--- util.h
+++ util.h
@@ -4,6 +4,7 @@
 #define UTIL_H
 
 #include <iostream>
+#include <sstream>
 #include <map>
 #include <vector>
 #include <string>
@@ -12,18 +13,33 @@
 #include "observer.h"
 #include "pieces.h"
 #include "player.h"
+#include "TextDisplay.h"
 
 using namespace std;
+
+struct Move; // forward-declaration
 
 enum class Colour { White, Black };
 
 struct Position {
     int row, col;
+
+    bool operator==(const Position &other) {
+        return (this->row == other.row && this->col == other.col);
+    }
 };
 
 struct Move {
     Position startPos, endPos;
     bool captured;
+
+    bool operator==(const Move &other) {
+        return (this->startPos == other.startPos && 
+        this->endPos == other.endPos && this->captured == other.captured);
+    }
 };
+
+// if a player resigns, return this move
+const extern Move RESIGNATION_MOVE = Move{{-1, -1}, {-1, -1}, false};
 
 #endif
