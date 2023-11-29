a21b9d3ddfeeede7a215ad4018a53068a92f5179
Amol Venkataraman <amolven16@gmail.com>
2023-11-28 21:03:55 -0500

    observer.cc comments

--- observer.cc
+++ observer.cc
@@ -1,7 +1,9 @@
 #include "util.h"
 #include "observer.h"
 
+// notify(pair, Piece *) - Calls the doNotify function to update and print the grid
 void Observer::notify(pair<int, int> pos, Piece *p)
 {
+    // NVI - Call the doNotify function
     doNotify(pos, p);
 }
