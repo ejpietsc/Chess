0df7e28a95b961efd0f6bcb5d326724d1be09b84
Imane Yacoubi <imaneyacoubi73@gmail.com>
2023-12-02 16:45:05 -0500

    still fixing ...

--- board.cc
+++ board.cc
@@ -117,13 +117,14 @@ bool Board::checkMovePiece(const Move &m) const
 // TODO - Add advanced logic (Double move, en passant, castling)
 bool Board::checkMoveEndPos(const Move &m) const
 {
+    Piece* p = getPiece(m.endPos);
     return (
         //? checked in pieces already?
         m.endPos.col >= 0 && // The end position is within bounds
         m.endPos.col < NUM_COLS &&
         m.endPos.row >= 0 &&
         m.endPos.row < NUM_ROWS &&
-        ((getPiece(m.endPos) == nullptr) || (getPiece(m.endPos)->getColour() != getPiece(m.startPos)->getColour())));
+        (!p || (p->getType() == PieceType::NULL_PIECE) || (p->getColour() != getPiece(m.startPos)->getColour())));
 }
 
 //! gets all moves for all pieces - even for human
@@ -498,12 +499,12 @@ Player *Board::getCurrPlayer() const
 
 Piece *Board::getPiece(const Position &pos) const
 {
-    return board[pos.col][pos.row].get();
+    return (board[pos.col][pos.row]).get();
 }
 
 Piece *Board::getPieceByCoords(int c, int r) const
 {
-    return board[c][r].get();
+    return (board[c][r]).get();
 }
 
 float Board::getScore(Colour clr)
